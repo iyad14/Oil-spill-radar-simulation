9954090c1d707c98f1e2857a44dd5f0110c56e73
iyad14 <iyadbaz46@gmail.com>
2022-06-14 13:49:18 +0200

    App base

--- Fractal Surface/Detection_probability.m
+++ Fractal Surface/Detection_probability.m
@@ -9,7 +9,7 @@
 %%  theta                   --> Incident angle of the electromagnetic wave to interface (given in degrees)]
 %%
 
-function probability_of_detection = Detection_probability(measured_reflectivity, M, frequency, ks, variance, E_air, temp, salinity, theta)
+function probability_of_detection = Detection_probability(R_oil, M, frequency, ks, variance, E_air, temp, salinity, theta)
    
 
     
@@ -17,7 +17,7 @@ function probability_of_detection = Detection_probability(measured_reflectivity,
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     E_water_prob = E_water(temp, salinity, frequency);
-    R_water = ones(size(measured_reflectivity)) * ((sqrt(E_air) - sqrt(E_water_prob))/(sqrt(E_air) + sqrt(E_water_prob)))^2;
+    R_water = ones(size(R_oil)) * ((sqrt(E_air) - sqrt(E_water_prob))/(sqrt(E_air) + sqrt(E_water_prob)))^2;
     R_water = abs(coherent_reflectivity(R_water, ks, theta));
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -26,17 +26,17 @@ function probability_of_detection = Detection_probability(measured_reflectivity,
         %%  Generating noise 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
      
-    trials = 3000;          % Quantity of noisy reflectivity values needed to be generated
+    trials = 1;          % Quantity of noisy reflectivity values needed to be generated
     
     % First & Second dimension  : image resolution
     % Third dimension  : number of frequencies used
     % Fourth dimension : Frequencies at which the scans are done
     
     % generates random values based on the given variance
-    noise = sqrt(variance) * randn(length(measured_reflectivity), size(measured_reflectivity, 1), length(frequency), trials, M); 
+    noise = sqrt(variance) * randn(length(R_oil), size(R_oil, 1), length(frequency), trials, M); 
   
     % Add the reflectivity values to the noise matrix, thus getting the noisy reflectivity values to be studied 
-    noisy_reflectivity = noise + measured_reflectivity;
+    noisy_reflectivity = noise + R_oil;
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -45,8 +45,8 @@ function probability_of_detection = Detection_probability(measured_reflectivity,
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     % Calculate the probabilty density functions
-    h1 = pdf("Normal", noisy_reflectivity, measured_reflectivity, variance);                                    % Oil
-    h2 = pdf("Normal", noisy_reflectivity, R_water, variance);                                                  % Water
+    h1 = pdf("Normal", noisy_reflectivity, R_oil, sqrt(variance));                                    % Oil
+    h2 = pdf("Normal", noisy_reflectivity, R_water, sqrt(variance));                                                  % Water
    
     % Multiply the pdf values at all frequencies(f) and scans(M)
     x = prod(h1, 5);
